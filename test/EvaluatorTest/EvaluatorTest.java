0a2bacea0a9f014d2ef38e65c4785174fafce927
leo <leoyu6@students.cs.ubc.ca>
2024-02-08 16:57:32 -0800

    merge test into eval work

--- test/EvaluatorTest/EvaluatorTest.java
+++ test/EvaluatorTest/EvaluatorTest.java
@@ -1,8 +1,10 @@
 package EvaluatorTest;
 
 import ast.*;
+import evaluator.Evaluator;
 import org.junit.jupiter.api.Test;
 
+import java.awt.event.PaintEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,10 +19,19 @@ public class EvaluatorTest {
         List<PropType> lampProperties = new ArrayList<PropType>();
         lampProperties.add(new EnumType(new Var("POWER"), new ArrayList<Var>(new Var("ON"), new Var("OFF"))));
 
+        List<Decl> decls = new ArrayList<>();
+        Program program = new Program(decls);
+
+
         Var lampVar = new Var("lamo");
-        Type lampType = new Type(lampVar, null, new List<PropType>() {
-        })
-        Device device1 = new Device()
+
+        Evaluator evaluator = new Evaluator();
+        StringBuilder stringBuilder = new StringBuilder();
+        evaluator.visit(stringBuilder, program);
+
+//        Type lampType = new Type(lampVar, null, new List<PropType>() {
+//        })
+//        Device device1 = new Device()
     }
 
 
