72612c9880ed1b54e65d252bcede01f99d6899ee
Ning114 <david.leng114@gmail.com>
2024-02-22 17:53:48 -0800

    Refactored AutomateSim to use an specified input/output stream if specified

--- src/ui/AutomateSim.java
+++ src/ui/AutomateSim.java
@@ -3,33 +3,67 @@ package ui;
 import model.*;
 import model.context.Context;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.Scanner;
 
 // final output of evaluation. the program can be run by calling run()
 public class AutomateSim {
-    private Scanner input = new Scanner(System.in);
+    private Scanner input;
+
+    // writes to our output set in the constructor for us
+    private PrintStream printer;
+
+    public AutomateSim() {
+        input = new Scanner(System.in);
+        printer = System.out;
+    }
+
+    // provide input using example here.
+    // https://stackoverflow.com/questions/6415728/junit-testing-with-simulated-user-input
+    public AutomateSim(InputStream in, ByteArrayOutputStream out) {
+        input = new Scanner(in);
+        printer = new PrintStream(out);
+    }
 
     public void run() {
-        System.out.println("What property would you like to change? (<device name>.<property name>)");
-        System.out.println("Or type !q to quit at any point.");
+        output("What property would you like to change? (<device name>.<property name>)");
+        output("Or type !q to quit at any point.");
         Property prop = typePropName();
 
-        System.out.println("What value would you like to change it to?");
+        if (prop == null) {
+            output("Ending program.");
+            return;
+        }
+
+        output("What value would you like to change it to?");
         typePropValue(prop);
 
-        run();
+
     }
+
+    private void output(String s) {
+        printer.println(s);
+    }
+
 
     private Property typePropName() {
         String in = input.next();
         if (in.strip().equals("!q")) {
-            System.exit(0);
+            return null;
+        }
+
+        if (in.length() == 0) {
+            output("No input found. Quitting");
+            return null;
         }
 
         String[] names = in.split("\\.");
 
         if (names.length != 2) {
-            System.out.println("Invalid syntax. Please try again. (<device name>.<property name>)");
+            output("Invalid syntax. Please try again. (<device name>.<property name>)");
             return typePropName();
         }
 
@@ -40,7 +74,7 @@ public class AutomateSim {
         if (device == null) {
             String error = String.format("The device %s does not exist. Please try again. " +
                     "(<device name>.<property name>)", deviceName);
-            System.out.println(error);
+            output(error);
             return typePropName();
         }
 
@@ -48,7 +82,7 @@ public class AutomateSim {
         if (prop == null) {
             String error = String.format("The property %s does not exist on the device %s. Please try again. " +
                     "(<device name>.<property name>)", propName, deviceName);
-            System.out.println(error);
+            output(error);
             return typePropName();
         }
 
@@ -58,11 +92,11 @@ public class AutomateSim {
     private void typePropValue(Property prop) {
         String in = input.next();
         if (in.strip().equals("!q")) {
-            System.exit(0);
+            return;
         }
 
         if (!prop.mutate(in)) {
-            System.out.println("The value is invalid. Please try again.");
+            output("The value is invalid. Please try again.");
             typePropValue(prop);
         }
     }
--- test/EvaluatorTest/EvaluatorTest.java
+++ test/EvaluatorTest/EvaluatorTest.java
@@ -8,8 +8,13 @@ import model.IfStmt;
 import model.SetStmt;
 
 import model.context.TestContext;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import ui.AutomateSim;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,6 +23,8 @@ import java.util.Set;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EvaluatorTest {
+
+
     @Test
     void basicRoomDecl() {
         // =============================================================================================================
@@ -120,6 +127,7 @@ public class EvaluatorTest {
 
         String output = stringBuilder.toString();
 
+
         System.out.println(output);
 
         HashMap<String, Set<String>> rooms =  TestContext.getRooms();
@@ -182,15 +190,134 @@ public class EvaluatorTest {
 
         assertNotNull(windowClose);
         assertEquals("CLOSED", windowClose.getVal());
+    }
+
+    @Test
+    void UiChangeDevicePropertyTest() {
+        // =============================================================================================================
+        // setup electronicType supertype.
+        // =============================================================================================================
+        Var electronicPower = new Var("ELECTRONIC_POWER");
+
+        EnumType electronicPropType = new EnumType(electronicPower, List.of(new Var("ON"), new Var("OFF")));
+
+        Type electronicType = new Type(new Var("electronicType"), null, List.of(electronicPropType));
+
+        // =============================================================================================================
+        // setup windowType and declare a window device
+        // =============================================================================================================
 
 
-//        assertTrue(output.contains(electronicType.getName().getText()));
-//        assertTrue(output.contains(lampOff.getText()));
-//        assertTrue(output.contains(windowStatus.getText()));
-//        assertTrue(output.contains(windowVar.getText()));
-//        assertTrue(output.contains(tvType.getName().getText()));
-//        assertTrue(output.contains(tv.getName().getText()));
-//        assertTrue(output.contains(bedroom.getName().getText()));
-//        assertTrue(output.contains(turnOnTv.getName().getText()));
+        Var windowStatus = new Var("WINDOW_STATUS");
+
+        EnumType windowPropType = new EnumType(windowStatus, List.of(new Var("OPEN"), new Var("CLOSED")));
+
+        Type windowType = new Type(new Var("windowType"), null, List.of(windowPropType));
+
+        List<PropVal> windowInitialState = List.of(new EnumVal(windowStatus.getText(), new Var("OPEN"), windowPropType));
+
+        Var windowVar = new Var("Window");
+        Device window = new Device(windowVar, windowType, windowInitialState);
+
+        // =============================================================================================================
+        // setup tvType and declare TV device
+        // =============================================================================================================
+        Var volVar = new Var("VOLUME");
+        Var disVar = new Var("DISPLAY_TEXT");
+        NumberType vol = new NumberType(volVar, 0, 100);
+        StringType dis = new StringType(disVar);
+
+
+        Type tvType = new Type(new Var("tvType"), electronicType, List.of(vol, dis, electronicPropType));
+
+        List<PropVal> tvInitialStates = List.of(
+                new EnumVal(electronicPower.getText(), new Var("OFF"), electronicPropType),
+                new NumberVal(volVar.getText(), "50", vol),
+                new StringVal(disVar.getText(), "DISPLAY TEST!!!", dis));
+
+        // Do not declare "POWER" enum because it inherits it from electronicType.
+
+        Var tvVar = new Var("Tv");
+        Device tv = new Device(tvVar, tvType, tvInitialStates);
+
+        // =============================================================================================================
+        // setup the room.
+        // =============================================================================================================
+        Room bedroom = new Room(new Var("Bedroom"), List.of(window, tv));
+
+        // =============================================================================================================
+        // setup some statements for the tv.
+        // =============================================================================================================
+        SetStatement setTvOn = new SetStatement(new DeviceProp(tvVar, electronicPower),
+                new EnumVal(electronicPower.getText(),
+                        new Var("ON"), electronicPropType));
+
+
+        SetStatement increaseTvVolume = new SetStatement(new DeviceProp(tvVar, volVar),
+                new NumberVal(volVar.getText(), "90", vol));
+
+
+        // =============================================================================================================
+        // setup the if statement that will trigger the previous set statements for tv.
+        // =============================================================================================================
+        IfStatement ifWindowClosesSetTvOn = new IfStatement(new DeviceProp(windowVar, windowStatus),
+                new EnumVal(windowStatus.getText(), new Var("CLOSED"), windowPropType), List.of(setTvOn, increaseTvVolume));
+
+        // =============================================================================================================
+        // setup the Action.
+        // =============================================================================================================
+
+        Action turnUpTvIfWindowCloses = new Action(new Var("Turn up TV if window closes"),
+                List.of(new DeviceProp(windowVar, windowStatus)),
+                List.of(ifWindowClosesSetTvOn));
+
+
+
+
+        // =============================================================================================================
+        // Start the program and populate the Model.Context for testing.
+        // =============================================================================================================
+
+        List<Decl> decls = new ArrayList<>();
+
+        decls.add(electronicType);
+        decls.add(windowType);
+        decls.add(tvType);
+        decls.add(bedroom);
+        decls.add(turnUpTvIfWindowCloses);
+
+        Program program = new Program(decls);
+
+        TestEvaluator evaluator = new TestEvaluator();
+        StringBuilder stringBuilder = new StringBuilder();
+        evaluator.visit(stringBuilder, program);
+
+        String output = stringBuilder.toString();
+
+        System.out.println(output);
+
+        HashMap<String, Set<String>> rooms =  TestContext.getRooms();
+        HashMap<String, model.Device> devices = Context.getDevices();
+        HashMap<String, model.Action> actions = TestContext.getActions();
+
+        String testInput = "Tv.ELECTRONIC_POWER\n" +
+                           "OFF\n" +
+                           "!q";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(testInput.getBytes());
+
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        AutomateSim ui = new AutomateSim(in, out);
+        ui.run();
+
+        // Get the bytes written to the ByteArrayOutputStream
+        byte[] outputBytes = out.toByteArray();
+
+        // Convert the bytes to a string and print it
+        String outputString = new String(outputBytes);
+        System.out.println("Output from PrintStream: \n" + outputString);
+
+
     }
 }
